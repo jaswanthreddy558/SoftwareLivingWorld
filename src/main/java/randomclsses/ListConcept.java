8b16a3d03c92a4ebba8a6574eea460cb95080766
Jaswanthreddy558 <JaswanthReddy454@gmail.com>
2022-07-30 21:24:45 -0400

    squash! Added filter

--- src/main/java/randomclsses/ListConcept.java
+++ src/main/java/randomclsses/ListConcept.java
@@ -1,18 +1,28 @@
 package randomclsses;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class ListConcept {
     public static void main(String[] args) {
-        Map<String, String> map = new HashMap<>();
-
-
+        List<String> list = ListConcept.getList();
+        Map<String, List<String>> map = new HashMap<>();
+        map.put("ListObject", list);
+        Iterator<Map.Entry<String, List<String>>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<String, List<String>> nextvalue = iterator.next();
+            System.out.println(nextvalue.getValue());
+        }
 
     }
 
+    public static List<String> getList() {
+        List<String> list = new ArrayList<>();
+        list.add("India");
+        list.add("London");
+        list.add("USA");
+        return list;
 
+    }
 }
 
 
